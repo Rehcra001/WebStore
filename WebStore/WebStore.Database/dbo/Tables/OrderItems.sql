026f508f803c74326b6860e4c49c6f0c8e2df964
Rechra001 <tkropf12345@gmail.com>
2024-02-05 08:18:44 +0200

    Added Orders and OrderItems tables to the database

---
+++ WebStore/WebStore.Database/dbo/Tables/OrderItems.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[OrderItems]
+(
+	OrderItemId INT NOT NULL PRIMARY KEY IDENTITY,
+	OrderId INT NOT NULL,
+	ProductId INT NOT NULL,
+	Quantity INT NOT NULL,
+	Price MONEY NOT NULL,
+	CONSTRAINT FK_OrderItems_Orders_OrderId FOREIGN KEY (OrderId) REFERENCES dbo.Orders(OrderId),
+	CONSTRAINT FK_OrderItems_Products_ProductId FOREIGN KEY (ProductId) REFERENCES dbo.Products(ProductId)
+)
---
+++ WebStore/WebStore.Database/dbo/Tables/Orders.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Orders]
+(
+	OrderId INT NOT NULL PRIMARY KEY IDENTITY,
+	CustomerId INT NOT NULL,
+	OrderDate DATETIME2 NOT NULL,
+	TotalPrice MONEY NOT NULL,
+	OrderConfirmed BIT DEFAULT(0) NOT NULL,
+	OrderShipped BIT DEFAULT(0) NOT NULL,
+	AddressId INT NOT NULL,
+	CONSTRAINT FK_Orders_Customers_CustomerId FOREIGN KEY (CustomerId) REFERENCES dbo.Customers(CustomerId),
+	CONSTRAINT FK_Orders_Addresses_AddressId FOREIGN KEY (AddressId) REFERENCES dbo.Addresses(AddressId)
+)
