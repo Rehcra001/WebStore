5dc6be9a933a03709e95abe901c9d2ac92de2472
Rechra001 <tkropf12345@gmail.com>
2024-03-03 09:38:14 +0200

    Customer Detail Workflow save

--- WebStore/WebStore.Web/Pages/CustomerProfile/CustomerDetail.razor.cs
+++ WebStore/WebStore.Web/Pages/CustomerProfile/CustomerDetail.razor.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Components;
+using System;
 using WebStore.DTO;
 using WebStore.WEB.Services.Contracts;
+using WebStore.WEB.Validators;
 
 namespace WebStore.WEB.Pages.CustomerProfile
 {
@@ -12,47 +15,182 @@ namespace WebStore.WEB.Pages.CustomerProfile
         [Inject]
         public IManageCustomerDetailLocalStorage ManageCustomerDetailLocalStorage { get; set; }
 
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
         [Parameter]
         public CustomerDTO Customer { get; set; } = new CustomerDTO();
 
         private List<AddressDTO> Addresses = new List<AddressDTO>();
 
-        private int count = 0;
+        private IList<List<string>> ValidationAddressErrors = new List<List<string>>();
+
+        private List<string> ValidationCustomerDetailErrors = new List<string>();
+
+        private List<string> ValidationNewAddressErrors = new List<string>();
+
+        private List<bool> ShouldDisplayAddress = new List<bool>();
+        private List<string> AddressDisplay = new List<string>();
+
+        private bool IsNewAddress = false;
+        private string NewAddressDisplay = "none";
+
+        private AddressDTO NewAddress { get; set; } = new AddressDTO();
 
         protected override void OnInitialized()
         {
             Addresses = Customer.AddressList.ToList();
+            //instantiate and error list for each address in Addresses
+            foreach (AddressDTO address in Addresses)
+            {
+                ValidationAddressErrors.Add(new List<string>());
+                ShouldDisplayAddress.Add(false);
+                AddressDisplay.Add("none");
+            }
+        }
+
+        private void ShowHideNewAddress()
+        {
+            IsNewAddress = !IsNewAddress;
+            NewAddress = new AddressDTO();
+            ValidationNewAddressErrors.Clear();
+            if (IsNewAddress)
+            {
+                NewAddressDisplay = "";
+            }
+            else
+            {
+                NewAddressDisplay = "none";
+            }
         }
 
         private async Task SaveCustomerDetail()
         {
-            UpdateCustomerDetailDTO updateCustomerDetailDTO = new UpdateCustomerDetailDTO
+            //Validate
+            ValidateCustomerDetail();
+
+            if (ValidationCustomerDetailErrors.Count == 0)
             {
-                CustomerId = Customer.CustomerId,
-                FirstName = Customer.FirstName,
-                LastName = Customer.LastName,
-                PhoneNumber = Customer.PhoneNumber,
-                EmailAddress = Customer.EmailAddress
-            };
+                UpdateCustomerDetailDTO updateCustomerDetailDTO = new UpdateCustomerDetailDTO
+                {
+                    CustomerId = Customer.CustomerId,
+                    FirstName = Customer.FirstName,
+                    LastName = Customer.LastName,
+                    PhoneNumber = Customer.PhoneNumber,
+                    EmailAddress = Customer.EmailAddress
+                };
 
-            await CustomerService.UpdateCustomerDetailAsync(updateCustomerDetailDTO);
+                await CustomerService.UpdateCustomerDetailAsync(updateCustomerDetailDTO);
 
-            await ManageCustomerDetailLocalStorage.SaveCustomerDetail(Customer);
+                await ManageCustomerDetailLocalStorage.SaveCustomerDetail(Customer);
+            }
+
         }
 
         private async Task SaveAddress(int index)
         {
-            //Save address
-            await CustomerService.UpdateCustomerAddressAsync(Addresses[index]);
+            ValidateCustomerAddress(index);
 
-            Customer.AddressList = Addresses;
+            if (ValidationAddressErrors[index].Count == 0)
+            {
+                //Save address
+                await CustomerService.UpdateCustomerAddressAsync(Addresses[index]);
 
-            await ManageCustomerDetailLocalStorage.SaveCustomerDetail(Customer);
+                Customer.AddressList = Addresses;
+
+                await ManageCustomerDetailLocalStorage.SaveCustomerDetail(Customer);
+            }
         }
 
         private async Task AddNewAddress()
         {
-            throw new NotImplementedException();
+            ValidateNewAddress();
+
+            if (ValidationNewAddressErrors.Count == 0)
+            {
+                //Save new address
+                AddressDTO address = await CustomerService.AddCustomerAddressAsync(NewAddress);
+                if (address == null || address.AddressId == 0)
+                {
+                    throw new Exception("Unable to save new Address");
+                }
+                else
+                {
+                    Addresses.Add(address);
+                    Customer.AddressList = Addresses;
+                    await ManageCustomerDetailLocalStorage.SaveCustomerDetail(Customer);
+
+                    //Add validation for new address
+                    ValidationAddressErrors.Add(new List<string>());
+                    ShouldDisplayAddress.Add(false);
+                    AddressDisplay.Add("none");
+
+                    //Clear new address
+                    NewAddress = new AddressDTO();
+                }
+
+            }
         }
+
+        private void ValidateCustomerDetail()
+        {
+            ValidationCustomerDetailErrors.Clear();
+
+            CustomerDetailValidator validationRules = new CustomerDetailValidator();
+            ValidationResult validationResult = validationRules.Validate(Customer);
+
+            if (validationResult.IsValid == false)
+            {
+                foreach (var failure in validationResult.Errors)
+                {
+                    ValidationCustomerDetailErrors.Add($"{failure.PropertyName} {failure.ErrorMessage}");
+                }
+            }
+        }
+
+        private void ValidateCustomerAddress(int index)
+        {
+            ValidationAddressErrors[index].Clear();
+
+            AddressValidator validationRules = new AddressValidator();
+            ValidationResult validationResult = validationRules.Validate(Addresses[index]);
+
+            if (validationResult.IsValid == false)
+            {
+                foreach (var failure in validationResult.Errors)
+                {
+                    ValidationAddressErrors[index].Add($"{failure.PropertyName} {failure.ErrorMessage}");
+                }
+            }
+        }
+
+        private void ValidateNewAddress()
+        {
+            ValidationNewAddressErrors.Clear();
+
+            AddressValidator validationRules = new AddressValidator();
+            ValidationResult validationResult = validationRules.Validate(NewAddress);
+
+            if (validationResult.IsValid == false)
+            {
+                foreach (var failure in validationResult.Errors)
+                {
+                    ValidationNewAddressErrors.Add($"{failure.PropertyName} {failure.ErrorMessage}");
+                }
+            }
+        }
+
+        private void ShowHideAddress(int index)
+        {
+            if (AddressDisplay[index] == "none")
+            {
+                AddressDisplay[index] = "";
+            }
+            else
+            {
+                AddressDisplay[index] = "none";
+            }
+        }
+
     }
 }
---
+++ WebStore/WebStore.Web/Validators/CustomerDetailValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using WebStore.DTO;
+
+namespace WebStore.WEB.Validators
+{
+    public class CustomerDetailValidator : AbstractValidator<CustomerDTO>
+    {
+        private CustomValidationRules _vr = new CustomValidationRules();
+
+        public CustomerDetailValidator()
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            //First Name
+            RuleFor(u => u.FirstName)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.")
+                .Length(2, 100).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength}.")
+                .Must(_vr.BeAValidName).WithMessage("{PropertyName} may only contain letters, space or dash characters.");
+
+            //Last Name
+            RuleFor(u => u.LastName)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.")
+                .Length(2, 100).WithMessage("{PropertyName} must be between {MinLength} and {MaxLength}.")
+                .Must(_vr.BeAValidName).WithMessage("{PropertyName} may only contain letters, space or dash characters.");
+
+            //Phone Number
+            RuleFor(u => u.PhoneNumber)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.");
+
+            //Email Address
+            RuleFor(u => u.EmailAddress)
+                .NotEmpty().WithMessage("{PropertyName} may not be empty.")
+                .EmailAddress().WithMessage("{PropertyName} is not valid.")
+                .Must(_vr.BeAValidEmailCharacter).WithMessage("{PropertyName} contains invalid characters! ';' or '--'");
+
+        }
+    }
+}
